6f30cc191bd4e20119080607c9cda153a7ba23e9
pedrh77 <66256107+pedrh77@users.noreply.github.com>
2020-11-06 14:03:52 -0300

    Add files via upload

---
+++ Projetos/Exercicio2/script2.js
@@ -0,0 +1,42 @@
+let Local = document.getElementById("Texto");
+
+function BtnAzul() {
+    Local.style.color = "#0000ff";
+}
+
+function BtnRosa() {
+    Local.style.color = "#FF1493";
+}
+
+function BtnRoxo() {
+    Local.style.color = "#4b0082";
+}
+
+function BtnAmarelo() {
+    Local.style.color = "#FFFF00";
+}
+
+function BtnLaranja() {
+    Local.style.color = "#FFA500";
+}
+let Fundo = document.getElementById("alt");
+
+function FundoP() {
+    Fundo.style.background = "#000000"
+}
+
+function FundoC() {
+    Fundo.style.background = "#808080"
+}
+function FundoB() {
+    Fundo.style.background = "#ffffff"
+}
+function FundoCC() {
+    Fundo.style.background = "#DCDCDC"
+}
+function FundoAC() {
+    Fundo.style.background = "#836FFF"
+}
+function FundoV() {
+    Fundo.style.background = "#008000"
+}
